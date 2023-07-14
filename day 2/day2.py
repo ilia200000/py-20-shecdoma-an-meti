d0455c9627e6ad325f58ef490ab1e3976ea93478
ilia200000 <139346188+ilia200000@users.noreply.github.com>
2023-07-14 16:48:39 +0400

    Add files via upload

---
+++ day 2/day2.py
@@ -0,0 +1,54 @@
+print(asi)
+print(asi)
+print(aslamazai)
+print(asi)
+print(kaba)
+print(acvia)
+print(gamoicani)
+print(vinaa)
+ 
+pasuxi=(simindi)
+
+
+
+print(pasuxi)
+
+
+
+
+value="54"
+print (value
+       
+       
+tbilisi=60
+
+print "60
+
+didgori="gori
+print (gori
+       
+       print (didgori
+              
+              print(hello)
+
+        print(89)
+    print "gori"
+value="super"
+print (supoer)
+print (value
+       
+       
+tbilisi=60
+
+print "60
+
+didgori="gori
+print (gori
+     
+    print (didgor 
+ print(hello)
+
+  print(89)
+ print "gori"
+value="super"
+print (supoer)
